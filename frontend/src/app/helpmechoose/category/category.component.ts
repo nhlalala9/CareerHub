ad2356dd3136cf4a818a451df25697de319b2456
kgopotso <kgopotso.maloma@dadevs.co.za>
2022-09-13 16:00:17 +0200

    career

--- frontend/src/app/helpmechoose/category/category.component.ts
+++ frontend/src/app/helpmechoose/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/model/category';
 import { CategoryService } from 'src/app/services/category.service';
 import { GetcategoryService } from 'src/app/services/getcategory.service';
 import { RoutingService } from 'src/app/services/routing.service';
@@ -11,7 +13,6 @@ import { RoutingService } from 'src/app/services/routing.service';
 export class CategoryComponent implements OnInit {
   public category$!: Observable<Category[]>;
 
-  constructor(private getCategory: GetcategoryService,public category: CategoryService) { }
 
   constructor(private getCategory: GetcategoryService, public routing: RoutingService) { }
 
