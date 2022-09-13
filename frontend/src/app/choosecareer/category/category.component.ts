76035b143bcff742d7a39ea4915c880b06ec267a
kgopotso <kgopotso.maloma@dadevs.co.za>
2022-09-13 15:55:54 +0200

    career

--- frontend/src/app/choosecareer/category/category.component.ts
+++ frontend/src/app/choosecareer/category/category.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor(private category: CategoryService) { }
+  constructor(private category: CategoryService, private route: Router) {}
 
   hold: any;
 
   ngOnInit(): void {
+    (this.category.browse = 'Browse Category'),
+      this.category.category().subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.hold = data;
+        },
+      });
+  }
 
-    this.category.browse = "Browse Category",
 
-   this.category.category().subscribe(
-     {
-     next:(data: any)=>{
-       console.log(data);
-       this.hold = data;
-     }
-   })
+
+
+
+
+
+
+
+  sendId(num: any) {
+   localStorage.setItem('careercategoryid', this.hold[num].id);
+
+    this.route.navigate(['/choose/career']);
   }
 }
