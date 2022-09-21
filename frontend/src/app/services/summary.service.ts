64466e907d1070ca6dff1114fc0f67f6d0a6d5ce
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-09-21 16:06:26 +0200

    database

--- frontend/src/app/services/summary.service.ts
+++ frontend/src/app/services/summary.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Salary } from '../model/salary';
 import {Summary} from '../model/summary';
 
@@ -12,10 +13,10 @@ export class SummaryService {
   constructor(private http:HttpClient) { }
 
   summary(careerpathId:any ): Observable<Summary[]>{
-    return this.http.get<Summary[]>(`http://localhost:3000/summary?careerpathid=${careerpathId}`)
+    return this.http.get<Summary[]>(`${environment.baseUrl}/getsummary/${careerpathId}`)
   }
   
   salary(careerpathId:any): Observable<Salary[]>{
-    return this.http.get<Salary[]>(`http://localhost:3000/Salaryimage?careerpathid=${careerpathId}`)
+    return this.http.get<Salary[]>(`${environment.baseUrl}/getsalary/${careerpathId}`)
   }
 }
