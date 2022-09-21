3df177dcf170627bd38dd82b86745c6f1d0e9df3
kgopotso <kgopotso.maloma@dadevs.co.za>
2022-09-21 11:23:54 +0200

    backend

--- backend/app/controllers/qualification.controller.js
+++ backend/app/controllers/qualification.controller.js
@@ -1,1 +1,21 @@
 const db = require ("../configs/db.config")
+
+exports.createQualification = (req, res) => {
+    const {name, image} = req.body
+
+    try {
+        db.query(
+          "INSERT INTO qualification (qualification, description, requirementsId) VALUES($1,$2,$3)",
+          [qualification, description,requirementsId],
+          (err) => {
+            if (err) {
+              res.status(400).json({ error: "Sorry we facing Technical issues" });
+            } 
+              res.status(201).json({ success: "Successful" }); 
+          }
+        );
+        } catch (error) {
+        res.status(500).json({ error: "database error" });
+      }
+    
+}
