819c287e5d12c6ef46d57d531317393a2b3b1eec
kgopotso <kgopotso.maloma@dadevs.co.za>
2022-09-21 14:25:41 +0200

    backened

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Careercategory } from '../model/careercategory';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class CategoryService {
   browse = '';
 
   category(): Observable<Careercategory[]>{
-    return this.http.get<Careercategory[]> (' http://localhost:3000/careercategory',{responseType: 'json'});
+    return this.http.get<Careercategory[]> (`${environment.baseUrl}/getcareercategory`,{responseType: 'json'});
   }
  
 }
