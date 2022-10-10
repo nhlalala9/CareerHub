54fe779b336b5d00a8b4d082de9134f22803cccb
kgopotso <kgopotso.maloma@dadevs.co.za>
2022-10-10 16:22:04 +0200

    suggestion

--- frontend/src/app/helpmechoose/suggestion/suggestion.component.ts
+++ frontend/src/app/helpmechoose/suggestion/suggestion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { CategoryService } from 'src/app/services/category.service';
 import { RoutingService } from 'src/app/services/routing.service';
 import { SuggestionService } from 'src/app/services/suggestion.service';
@@ -10,12 +12,15 @@ import { SuggestionService } from 'src/app/services/suggestion.service';
 })
 export class SuggestionComponent implements OnInit {
 
-  constructor(public category: CategoryService, public routing: RoutingService, private suggested: SuggestionService) { }
+  constructor(public category: CategoryService, public routing: RoutingService, private suggested: SuggestionService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.category.browse = ''
     this.routing.dynamic = 'help'
   }
 
-
+  public firstCareer$ = this.router.paramMap.pipe(
+    switchMap((params)=>
+    )
+  )
 }
