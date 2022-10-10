12ae54d0626acacbe8ab280aebfb822480c14371
shaggy <shaggy.sambo@dadevs.co.za>
2022-10-10 16:08:51 +0200

    multi question page

--- frontend/src/app/helpmechoose/question/question.component.ts
+++ frontend/src/app/helpmechoose/question/question.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { values } from 'cypress/types/lodash';
+import { shareReplay, switchMap } from 'rxjs';
 import { Questions } from 'src/app/model/questions';
 import { Score } from 'src/app/model/score';
 import { CategoryService } from 'src/app/services/category.service';
+import { QuestionService } from 'src/app/services/question.service';
 import { RoutingService } from 'src/app/services/routing.service';
 
 @Component({
@@ -14,245 +17,49 @@ export class QuestionComponent implements OnInit {
   constructor(
     public category: CategoryService,
     public routing: RoutingService,
-    private route: Router
-  ) {}
+    private route: Router,
+    private question: QuestionService,
+    private router: ActivatedRoute
+  ) {
+    this.getQuestion$.subscribe(
+      {
+        next: (questions: Questions[])=>{
+          console.log(questions)
+          this.show$ = questions
+        }
+      }
+    )
+  }
 
-  show$: Questions[] = [
-    {
-      id: 1,
-      question: 'Which can replace the question mark?',
-      answer: [
-        {
-          option: 'nice',
-          correct: false,
-          image:
-            'https://cdn.discordapp.com/attachments/1009769128179019867/1024621865865588797/Screenshot_2022-09-28_at_12.00.37.png',
-        },
-        {
-          option: 'not good',
-          correct: false,
-          image:
-            'https://cdn.discordapp.com/attachments/1009769128179019867/1024621866276626493/Screenshot_2022-09-28_at_12.00.48.png',
-        },
-        {
-          option: 'bad',
-          correct: true,
-          image:
-            'https://cdn.discordapp.com/attachments/1009769128179019867/1024621866750595132/Screenshot_2022-09-28_at_12.00.58.png',
-        },
-        {
-          option: 'nonsense',
-          correct: false,
-          image:
-            'https://cdn.discordapp.com/attachments/1009769128179019867/1024621867119685662/Screenshot_2022-09-28_at_12.01.06.png',
-        },
-      ],
-      type: 3,
-      image:
-        'https://cdn.discordapp.com/attachments/1009769128179019867/1024620618089517066/Screenshot_2022-09-28_at_11.56.07.png',
-      careerpathId: 1,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you choose?',
-      answer: [
-        {
-          option: 'a',
-          correct: false,
-          image:
-            'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-        },
-        {
-          option: 'b',
-          correct: true,
-          image:
-            'https://thumbs.dreamstime.com/b/alone-sunset-man-near-cherry-tree-87689017.jpg',
-        },
-        {
-          option: 'c',
-          correct: false,
-          image:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_VY6_afhq42uhghEZmPs1rHOSxnVANVCVq6JfTF3p4e4Rm82eUuDtj1IWuPvUVuS0u0A&usqp=CAU',
-        },
-      ],
-      type: 2,
-      image: '',
-      careerpathId: 1,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you choose?',
-      answer: [
-        {
-          option: 'a',
-          correct: false,
-          image:
-            'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-        },
-        {
-          option: 'b',
-          correct: true,
-          image:
-            'https://thumbs.dreamstime.com/b/alone-sunset-man-near-cherry-tree-87689017.jpg',
-        },
-        {
-          option: 'c',
-          correct: false,
-          image:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_VY6_afhq42uhghEZmPs1rHOSxnVANVCVq6JfTF3p4e4Rm82eUuDtj1IWuPvUVuS0u0A&usqp=CAU',
-        },
-      ],
-      type: 2,
-      image: '',
-      careerpathId: 1,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: true, image: '' },
-        { option: 'No', correct: false, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://media.istockphoto.com/photos/villefranche-on-sea-in-evening-picture-id1145618475?k=20&m=1145618475&s=612x612&w=0&h=_mC6OZt_eWENYUAZz3tLCBTU23uvx5beulDEZHFLsxI=',
-      careerpathId: 2,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://photographylife.com/wp-content/uploads/2017/01/Best-of-2016-Nasim-Mansurov-20.jpg',
-      careerpathId: 2,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://s4.reutersmedia.net/resources/r/?m=02&d=20211029&t=2&i=1579603325&w=&fh=545&fw=810&ll=&pl=&sq=&r=2021-10-29T223613Z_42247_MRPRC2SFQ97INDQ_RTRMADP_0_EGYPT-HERITAGE-MODERN-ART',
-      careerpathId: 2,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      careerpathId: 3,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYVqkt5GU65m9NP_-JhL6scOz49qL-ff9h6D1Lbh45cM-gbJJuF1whVqw9DRSFezyrS6Y&usqp=CAU',
-      careerpathId: 3,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaQBMJJw3CsZbJ1rfbddHde2qbPjq8gmWT_S8058muJ6qkCyq4mtgmWODALqExozWQ79w&usqp=CAU',
-      careerpathId: 3,
-      categoryId: 1,
-    },
-    
+  public getQuestion$ = this.router.paramMap.pipe(
+    switchMap((params)=>
+      this.question.getQuestion(params.get('id')).pipe(shareReplay(1))
+    )
+  )
 
-
-
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaQBMJJw3CsZbJ1rfbddHde2qbPjq8gmWT_S8058muJ6qkCyq4mtgmWODALqExozWQ79w&usqp=CAU',
-      careerpathId: 6,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'Which can replace the question mark?',
-      answer: [
-        { option: '', correct: true, image: 'https://cdn.discordapp.com/attachments/1009769128179019867/1024624511972278272/Screenshot_2022-09-28_at_12.11.21.png' },
-        { option: '', correct: false, image: 'https://cdn.discordapp.com/attachments/1009769128179019867/1024624512588845067/Screenshot_2022-09-28_at_12.11.30.png' },
-        { option: '', correct: true, image: 'https://cdn.discordapp.com/attachments/1009769128179019867/1024624513008279562/Screenshot_2022-09-28_at_12.11.41.png' },
-      ],
-      type: 3,
-      image:
-        'https://cdn.discordapp.com/attachments/1009769128179019867/1024624511456391208/Screenshot_2022-09-28_at_12.10.10.png',
-      careerpathId: 6,
-      categoryId: 1,
-    },
-    {
-      id: 1,
-      question: 'what do you want?',
-      answer: [
-        { option: 'Yes', correct: false, image: '' },
-        { option: 'No', correct: true, image: '' },
-      ],
-      type: 1,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaQBMJJw3CsZbJ1rfbddHde2qbPjq8gmWT_S8058muJ6qkCyq4mtgmWODALqExozWQ79w&usqp=CAU',
-      careerpathId: 6,
-      categoryId: 1,
-    },
-  ];
+  
 
   scores: Score[] = [];
   score: number = 0;
   currentQuiz: number = 0;
   answerSelected = false;
+  show$: Questions[] = [];
 
   ngOnInit(): void {
     this.category.browse = '';
     this.routing.dynamic = 'help';
+    this.routing.home = 'active';
+
+
   }
 
   answer(correct: boolean) {
     console.log(correct);
   }
 
-  getQuizzes() {
-    return this.show$;
-  }
 
   ngDoCheck() {
-    if (this.currentQuiz === this.show$.length) {
+    if (this.currentQuiz === this.show$.length && this.show$.length != 0) {
       this.route.navigate([`/help/suggestion/${this.career}/${this.career2}`]);
     }
   }
@@ -273,12 +80,12 @@ export class QuestionComponent implements OnInit {
           this.score++;
           console.log(`this is second ${option}`);
           this.scoreMap.set(careerpathId, this.score);
-          console.log(this.scoreMap);
+        
         } else if (option && this.count > 3) {
           this.score++;
           console.log(`this is second but greater ${option}`);
           this.scoreMap.set(careerpathId, this.score);
-          console.log(this.scoreMap);
+        
           this.count = 0;
           this.score = 0;
         }
@@ -289,9 +96,6 @@ export class QuestionComponent implements OnInit {
           this.count = this.count + 1;
           this.score = this.score + 1;
           this.scoreMap.set(careerpathId, this.score);
-          console.log(this.scoreMap);
-
-          console.log(`this is third  ${option}`);
         }
       }
     }
