78c6813409d7469650bc9f934caafc7ee40a30b9
shaggy <shaggy.sambo@dadevs.co.za>
2022-10-10 16:16:29 +0200

    multi question page

---
+++ backend/app/controllers/suggestion.controller.js
@@ -0,0 +1,21 @@
+exports.getsuggested = (req, res) => {
+    const careerpathId = req.params.id;
+    try {
+      db.query(
+        "SELECT * FROM careerpath WHERE careerpathid = $1",
+        [careerpathId],
+        (err, results) => {
+          if (err) {
+            res
+              .status(400)
+              .json({ error: "sorry,we are still fixing technical issues" });
+          }
+          res.status(200).json(results.rows);
+        }
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "sorry,we are still fixing technical issues" });
+    }
+  };
