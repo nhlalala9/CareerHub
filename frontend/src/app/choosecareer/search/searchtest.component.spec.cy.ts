3d2e00f0091a20c8637d0271f20616414470513b
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-10-14 10:01:29 +0200

    search test

---
+++ frontend/src/app/choosecareer/search/searchtest.component.spec.cy.ts
@@ -0,0 +1,20 @@
+import { TestBed } from "@angular/core/testing";
+import { CategoryService } from "src/app/services/category.service";
+import { GetcareerService } from "src/app/services/getcareer.service";
+import { RoutingService } from "src/app/services/routing.service";
+import { SearchComponent } from "./search.component";
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { Input } from "@angular/core";
+
+describe("testing the search component", () => {
+
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule], 
+        providers: [RoutingService, CategoryService, GetcareerService]
+      }));
+
+    it('mounts', () => {
+        cy.mount(SearchComponent)
+        cy.get('Input')
+    });
+})
