facdf7a32d82bd7ad7c37f6b27d20f0e4ac59613
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-11-01 08:55:07 +0200

    an and a

--- frontend/src/app/choosecareer/requirements/requirements.component.ts
+++ frontend/src/app/choosecareer/requirements/requirements.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { lowerCase } from 'cypress/types/lodash';
 import { map, Observable, shareReplay, switchMap, tap } from 'rxjs';
 import { Qualification } from 'src/app/model/qualification';
 import { Requirements } from 'src/app/model/requirements';
@@ -22,13 +23,21 @@ export class RequirementsComponent implements OnInit {
     private qualify: QualificationService
   ) {
     route.params.subscribe({
-      next: (params) =>{
+      next: (params) => {
         this.careerpathName = params['name']
+        if(this.careerpathName[0] == 'A' || (this.careerpathName[0]) == 'E' || (this.careerpathName[0]) == 'I' || (this.careerpathName[0]) == 'O' || (this.careerpathName[0]) == 'U'){
+          this.title = 'an';
+        }else{
+          this.title = 'a';
+        }
       }
-      });
+    });
   }
 
+
+
   careerpathName: any;
+  title: any;
 
   public requirements$ = this.route.paramMap.pipe(
     switchMap((params) =>
@@ -49,8 +58,8 @@ export class RequirementsComponent implements OnInit {
     this.routing.search = '';
 
     this.qualification$ = this.requirements$.pipe(
-      switchMap((values: Requirements[])=>
-      this.qualify.getQualification(values[0].id).pipe(shareReplay(1))
+      switchMap((values: Requirements[]) =>
+        this.qualify.getQualification(values[0].id).pipe(shareReplay(1))
       )
     )
   }
